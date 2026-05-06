50505bbf48c22f5f5fdad4e30eacab484bfc0c36
Collin White <cjwhite101@icloud.com>
2026-05-06 15:19:27 -0400

    Allowed users to specify a page range to extract text from

--- extractText.py
+++ extractText.py
@@ -33,17 +33,30 @@ logging.info("PDF file got, creating reader object...")
 reader = PdfReader(pdf_path)
 logging.debug("Prompting page to extract text from...")
 print(f"You entered {pdf_path} as the file to extract text from.")
-page_to_read = input(f"What page would you like to extract text from? (1 - {len(reader.pages)}) (if all, type -1): ")
+page_to_read = input(f"What page would you like to extract text from? (1 - {len(reader.pages)}) (if all, type -1) (to specify range, type -2): ")
 
 extracted_text = []
 
-if((int(page_to_read) < 0)):
+if((int(page_to_read) == -1)):
     logging.info(f"User input: {page_to_read}, extracting all pages text")
     i = 0
     for page in reader.pages:
         logging.debug(f"Extracting text for page {i}")
         extracted_text.append(page.extract_text())
         i = i + 1
+        
+elif((int(page_to_read) == -2)):
+    #ask to specify page range
+    logging.info(f"User input: {page_to_read}, prompting for page range to extract text from...")
+    page_range = input('What page range would like to extract text from? (ex: 1-3, 5-7, etc.) (-1): ')
+    nums = page_range.split('-')
+    begIndex = int(nums[0]) - 1
+    endIndex = int(nums[1]) - 1
+    logging.info(f"Extracting text from page range {begIndex} to {endIndex}")
+    for i in range(begIndex, endIndex + 1):
+        logging.debug(f"Extracting text for page {i}")
+        page = reader.pages[i]
+        extracted_text.append(page.extract_text())
 
 else:
     logging.info(f"Extracting text from page {page_to_read}")
